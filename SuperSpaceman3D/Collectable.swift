42525b514598522f22af53064286c687ca06d08d
Bret Williams <bret@brets-mbp.tx.rr.com>
2018-05-27 16:40:42 -0500

    added more shape collectables

--- SuperSpaceman3D/Collectable.swift
+++ SuperSpaceman3D/Collectable.swift
@@ -15,7 +15,7 @@ class Collectable {
         
         let pyramid = SCNPyramid(width: 3.0, height: 6.0, length: 3.0)
         let pyramidNode = SCNNode(geometry: pyramid)
-        let position = SCNVector3Make(30, 0, -40)
+        let position = SCNVector3Make(0, 0, 200)
         
         pyramidNode.position = position
         pyramidNode.name = "pyramid"
@@ -30,7 +30,7 @@ class Collectable {
         
         let sphere = SCNSphere(radius: 6.0)
         let sphereNode = SCNNode(geometry: sphere)
-        let position = SCNVector3Make(35, 0, -60)
+        let position = SCNVector3Make(0, 6, -200)
         
         sphereNode.name = "sphere"
         sphereNode.position = position
@@ -38,6 +38,66 @@ class Collectable {
         sphereNode.geometry?.firstMaterial?.shininess = 1.0
         
         return sphereNode
+        
+    }
+    
+    class func cylinderNode() -> SCNNode {
+        
+        let cylinder = SCNCylinder(radius: 3, height: 8)
+        let cylinderNode = SCNNode(geometry: cylinder)
+        let position = SCNVector3Make(300, 8, 300)
+        
+        cylinderNode.name = "cylinder"
+        cylinderNode.position = position
+        cylinderNode.geometry?.firstMaterial?.diffuse.contents = UIColor.green
+        cylinderNode.geometry?.firstMaterial?.shininess = 1.0
+        
+        return cylinderNode
+        
+    }
+    
+    class func torusNode() -> SCNNode {
+        
+        let torus = SCNTorus(ringRadius: 7, pipeRadius: 2)
+        let torusNode = SCNNode(geometry: torus)
+        let position = SCNVector3Make(-300, 0, 300)
+        
+        torusNode.name = "torus"
+        torusNode.position = position
+        torusNode.geometry?.firstMaterial?.diffuse.contents = UIColor.orange
+        torusNode.geometry?.firstMaterial?.shininess = 1.0
+        
+        return torusNode
+        
+    }
+    
+    class func tubeNode() -> SCNNode {
+        
+        let tube = SCNTube(innerRadius: 1, outerRadius: 1.5, height: 2)
+        let tubeNode = SCNNode(geometry: tube)
+        let position = SCNVector3Make(-200, 1.5, 0)
+        
+        tubeNode.position = position
+        tubeNode.name = "tube"
+        tubeNode.geometry?.firstMaterial?.diffuse.contents = UIColor.yellow
+        tubeNode.geometry?.firstMaterial?.shininess = 1.0
+        
+        return tubeNode
+        
+    }
+    
+    class func boxNode() -> SCNNode {
+        
+        let box = SCNBox(width: 3, height: 3, length: 3, chamferRadius: 0)
+        let boxNode = SCNNode(geometry: box)
+        let position = SCNVector3Make(200, 3, -0)
+        
+        boxNode.position = position
+        boxNode.name = "box"
+        boxNode.geometry?.firstMaterial?.diffuse.contents = UIColor.brown
+        boxNode.geometry?.firstMaterial?.shininess = 1.0
+        
+        return boxNode
         
     }
     
--- SuperSpaceman3D/GameViewController.swift
+++ SuperSpaceman3D/GameViewController.swift
@@ -19,8 +19,13 @@ class GameViewController: UIViewController {
      
         mainScene = createMainScene()
         mainScene!.rootNode.addChildNode(createFloorNode())
-        //mainScene!.rootNode.addChildNode(Collectable.pyramidNode())
+        mainScene!.rootNode.addChildNode(Collectable.pyramidNode())
         mainScene!.rootNode.addChildNode(Collectable.sphereNode())
+        mainScene!.rootNode.addChildNode(Collectable.boxNode())
+        mainScene!.rootNode.addChildNode(Collectable.tubeNode())
+        mainScene!.rootNode.addChildNode(Collectable.cylinderNode())
+        mainScene!.rootNode.addChildNode(Collectable.torusNode())
+        
         
         let sceneView = self.view as! SCNView
         sceneView.scene = mainScene
